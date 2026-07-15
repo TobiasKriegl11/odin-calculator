027e35be3cd233e2e8208273f61aa37df530c041
TobiasKriegl11 <241426819+TobiasKriegl11@users.noreply.github.com>
2026-07-15 06:56:12 +0200

    Add basic math functions
    Add three variables for calculator operation

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
+};
+
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current)
+};
+
+const divide = function (array) {
+  return array.reduce((dividend, divisor) => dividend / divisor)
+};
+
+let firstNumber = null;
+let secondNumber = null;
+let operatorSymbol = null;
